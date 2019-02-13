13a17c7d4be20f99391cc60960416aff8ca5cb5c
loror <Lorrer@126.com>
2019-02-14 00:07:07 +0800

    Loror

--- app/src/main/java/com/loror/lororutil_compiler/MainActivity.java
+++ app/src/main/java/com/loror/lororutil_compiler/MainActivity.java
@@ -11,6 +11,8 @@ import android.widget.Toast;
 import com.loror.lororUtil.view.Click;
 import com.loror.lororUtil.view.Find;
 import com.loror.lororUtil.view.ItemClick;
+import com.loror.lororUtil.view.ItemLongClick;
+import com.loror.lororUtil.view.LongClick;
 import com.loror.lororUtil.view.ViewUtil;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ViewUtil.find(this);
+        ViewUtil.click(this);
         textView.setText("显示改变");
         listView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.test_list_item, new String[]{"1", "2", "3"}));
     }
@@ -34,8 +37,18 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, "点击了text", Toast.LENGTH_SHORT).show();
     }
 
+    @LongClick(id = R.id.text)
+    public void longCick(View view) {
+        Toast.makeText(this, "长按了text", Toast.LENGTH_SHORT).show();
+    }
+
     @ItemClick(id = R.id.list)
     public void itemClick(View v, int position) {
         Toast.makeText(this, "点击了list:" + position, Toast.LENGTH_SHORT).show();
+    }
+
+    @ItemLongClick(id = R.id.list)
+    public void itemLongClick(View v, int position) {
+        Toast.makeText(this, "长按了list:" + position, Toast.LENGTH_SHORT).show();
     }
 }
